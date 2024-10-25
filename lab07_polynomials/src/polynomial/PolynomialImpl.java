a32d65ac9d6996e2dfab94bfa82ef39b0e7cab6c
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-25 14:55:09 -0400

    checkstyle adjustment

--- lab07_polynomials/src/polynomial/PolynomialImpl.java
+++ lab07_polynomials/src/polynomial/PolynomialImpl.java
@@ -20,11 +20,10 @@ public class PolynomialImpl implements Polynomial {
     }
   }
 
-	@Override
-	public Polynomial add(Polynomial other) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public Polynomial add(Polynomial other) throws IllegalArgumentException {
+    return null;
+  }
 
 	@Override
 	public void addTerm(int coefficient, int power) throws IllegalArgumentException {
