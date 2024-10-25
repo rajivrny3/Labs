44242537789db439ef4423f03c3a6340a8d37ecd
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-25 14:01:44 -0400

    Created Polynomial imp1

--- lab07_polynomials/src/polynomial/PolynomialImp1.java
+++ lab07_polynomials/src/polynomial/PolynomialImp1.java
@@ -1,5 +1,41 @@
 package polynomial;
 
-public class PolynomialImp1 {
+public class PolynomialImp1 implements Polynomial {
+
+	@Override
+	public Polynomial add(Polynomial other) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTerm(int coefficient, int power) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isSame(Polynomial poly) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double evaluate(double x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCoefficient(int power) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDegree() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
