44b6c002ede0f2805f9b876328309b8311359f87
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-27 00:14:42 -0400

    finished setup

--- lab07_polynomials/test/test/PolynomialTest.java
+++ lab07_polynomials/test/test/PolynomialTest.java
@@ -17,11 +17,28 @@ public class PolynomialTest {
   private Polynomial negative;
   private Polynomial numericTerm;
   
+  /**
+   * Sets up objects to test on for PolynomialTest.
+   */
   @Before
   public void setup() {
     empty = new PolynomialImpl();
     
-    poly =  new PolynomialImpl()
+    poly =  new PolynomialImpl(4, 3, 
+        new PolynomialImpl(3, 1, 
+            new PolynomialImpl(-5, 0, new 
+                PolynomialImpl())));
+    
+    numericTerm = new PolynomialImpl(-102, 0, new PolynomialImpl());
+    
+    negative = new PolynomialImpl(-3, 4, 
+        new PolynomialImpl(-2, 5, 
+            new PolynomialImpl(-5, 0, new PolynomialImpl(-2, 4, 
+                new PolynomialImpl(11, 1, 
+                    new PolynomialImpl())))));
+    
+    differentDegree = new PolynomialImpl(3, 5, 
+        new PolynomialImpl(2, 2, new PolynomialImpl()));
   }
   
   @Test
