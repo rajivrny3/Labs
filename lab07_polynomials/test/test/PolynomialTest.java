de414f761fa4e5fda2f55260f87b456f59d623e0
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-26 23:44:38 -0400

    Made fields for polynomial test

--- lab07_polynomials/test/test/PolynomialTest.java
+++ lab07_polynomials/test/test/PolynomialTest.java
@@ -1,14 +1,28 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import polynomial.Polynomial;
+import polynomial.PolynomialImpl;
 
+/**
+ * Creates a test class for Polynomial.
+ */
 public class PolynomialTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+  private Polynomial poly;
+  private Polynomial empty;
+  private Polynomial differentDegree;
+  private Polynomial negative;
+  private Polynomial numericTerm;
+  
+  @Before
+  public void setup() {
+    
+  }
+  
+  @Test
+  public void test() {
+  }
 }
